84378efcae152afb40c2ae7241a5047a5d83097b
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2023-04-13 16:41:27 -0400

    push

--- radius.js
+++ radius.js
@@ -2,20 +2,14 @@ function getRadius(){
 
   console.log('getting zipcode from api');
 
-   var btn = document.getElementById('zipcode');
-   btn.addEventListener("click", () => {
-
   const apiKey = 'DemoOnly00zTU3R15kUfDWOw8o0haOVmTlwAdcMLbsBvDPVVlLvdlPo2lyz9pwBm';
   const zipCode = '02215';
   const distance = '5';
   const units = 'mile';
 
- fetch(`https://www.zipcodeapi.com/rest/${apiKey}/radius.json/${zipCode}/${distance}/${units}`)
-   .then(response => response.json())
+  fetch('https://www.zipcodeapi.com/rest/'+apiKey+'/radius.json/'+zipCode+'/'+distance+'/'+units, {mode:'no-cors'})
+   .then(response => console.log(response.json))
    .then(data => console.log(data))
-   .catch(error => console.error(error));
+   .catch(error => console.error(error))
 
- });
-
-}
-window.onload = getRadius;
+ }
