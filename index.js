2762d72efb0fbae2079d9caaacc3a94405c12c01
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-13 11:10:05 -0400

    Boston Longitude and Latitude

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 42.3601, lng: -71.0589 },
     zoom: 8,
   });
 }
