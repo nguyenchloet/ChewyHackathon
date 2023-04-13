b65777f0c5f21029604faf7eea7ada057f8b51df
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-13 16:01:02 -0400

    get zipcode input from user with jQuery

--- map.js
+++ map.js
@@ -18,14 +18,24 @@ fetch('./clinic-info.json')
   .catch(error => console.log(error));
 */
 
+/* get data from JSON file */
 $(document).ready(function() {
   $.getJSON('clinic-info.json', function(data) {
       //console.log(data);
       $.each(data, function(key, value) { 
           //console.log(key, value);
-          console.log(value.clinic_name);
-          console.log(value.zip);
+          //console.log(value.clinic_name);
+          //console.log(value.zip);
       });
+  });
+  /* get user input zip code */
+  $(document).ready(function(){
+    // Get value on button click and show alert
+    $("#zip-button").click(function(){
+        var zip = $("#zip-input").val();
+        console.log(zip);
+    });
   });
 });
             
+
--- zip.js
+++ zip.js
@@ -1,6 +1,7 @@
+/*
 function getZip() {
     var code = document.getElementById("zipcode-input").value;
     document.getElementById("zip-received").innerHTML = 'The user input zipcode is: ' + code;
 }
-
+*/
 
