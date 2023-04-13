b85fd93331e4a8fe7b92c86edeacf0970d08b85b
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-13 12:18:48 -0400

    Zip code user input accepted

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-let map;
-
-async function initMap() {
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 42.3601, lng: -71.0589 },
-    zoom: 8,
-  });
+function getZip() {
+    var variable = document.getElementById("zipcode-input").value;
+    document.getElementById("zip-received").innerHTML = 'The user input zipcode is: ' + variable;
 }
-
-initMap();
---
+++ map.js
@@ -0,0 +1,14 @@
+let map;
+
+async function initMap() {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 42.3601, lng: -71.0589 },
+    zoom: 8,
+  });
+}
+
+initMap();
+
