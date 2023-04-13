16b1aa0a2a1a5e53bd9d41a7d131c1e37a186be3
diyagandhi15 <123668392+diyagandhi15@users.noreply.github.com>
2023-04-13 19:10:51 -0400

    Update radius.js

--- radius.js
+++ radius.js
@@ -1,3 +1,19 @@
+// function getRadius(){
+
+//   console.log('getting zipcode from api');
+
+//   const apiKey = 'DemoOnly00zTU3R15kUfDWOw8o0haOVmTlwAdcMLbsBvDPVVlLvdlPo2lyz9pwBm';
+//   const zipCode = '02215';
+//   const distance = '5';
+//   const units = 'mile';
+
+//   fetch('https://www.zipcodeapi.com/rest/'+apiKey+'/radius.json/'+zipCode+'/'+distance+'/'+units, {mode:'no-cors'})
+//    .then(response => console.log(response.json))
+//    .then(data => console.log(data))
+//    .catch(error => console.error(error))
+
+//  }
+
 function getRadius(){
 
   console.log('getting zipcode from api');
@@ -8,8 +24,11 @@ function getRadius(){
   const units = 'mile';
 
   fetch('https://www.zipcodeapi.com/rest/'+apiKey+'/radius.json/'+zipCode+'/'+distance+'/'+units, {mode:'no-cors'})
-   .then(response => console.log(response.json))
-   .then(data => console.log(data))
-   .catch(error => console.error(error))
-
- }
+    .then(response => response.json())
+    .then(data => {
+      const results = data.zip_codes.map(zip => ({zipcode: zip.zip_code, distance: zip.distance}));
+      const sortedResults = results.sort((a, b) => a.distance - b.distance);
+      console.log(sortedResults);
+    })
+    .catch(error => console.error(error));
+}
