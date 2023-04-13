c7f3c38f1364167df3531c7be6b509292d212990
ctn <35670719+TNg018@users.noreply.github.com>
2023-04-13 12:32:55 -0400

    Get Data from json file

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ function getZip() {
     var variable = document.getElementById("zipcode-input").value;
     document.getElementById("zip-received").innerHTML = 'The user input zipcode is: ' + variable;
 }
+
